a1d84959818a0e1c8bff447de6a1620f7e88a8c2
Thanvi07-hp <rshettythanvi@gmail.com>
2025-04-16 10:40:20 +0530

    updates

--- hrms-backend/test.js
+++ hrms-backend/test.js
@@ -1,8 +1,8 @@
 // test.js
 const bcrypt = require('bcrypt');
 
-const storedHash = "$2b$10$YtOi4l6v/h2VXgVUSZ3.y.cDALacN6HccJGQHRrfsyc.83bV.BRPK"; // hash from DB
-const testPassword = "t123"; // the password you want to test
+const storedHash = ""; // hash from DB
+const testPassword = ""; // the password you want to test
 
 async function checkPassword() {
   try {
